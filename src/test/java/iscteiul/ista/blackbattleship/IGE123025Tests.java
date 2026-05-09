5586011bd0969cdb0c32821ca6930a4fbf27f766
jcjesus45 <jc.amaro.jesus@gmail.com>
2026-05-09 19:45:04 +0100

    US17 Test Issue: #1 User Story successfully tested: US17 - Deteção de navio afundado

--- src/test/java/iscteiul/ista/blackbattleship/IGE123025Tests.java
+++ src/test/java/iscteiul/ista/blackbattleship/IGE123025Tests.java
@@ -1,6 +1,7 @@
 package iscteiul.ista.blackbattleship;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
@@ -9,10 +10,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.util.Random;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Configuration.timeout;
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class IGE123025Tests {
 
@@ -58,7 +61,57 @@ public class IGE123025Tests {
                 .shouldBe(visible);
     }
 
-    public void closePopup() {
+    @Test
+    public void sunkenShipDetection() {
+        sleep(3000);
+
+        $x("//*[contains(text(), 'robot')]")
+                .shouldBe(visible)
+                .click();
+
+        sleep(2000);
+
+        $x("//*[@id='mat-select-serverApp0']")
+                .shouldBe(visible)
+                .click();
+
+        sleep(2000);
+
+        $x("//div[contains(text(),'Battleship')]")
+                .shouldBe(visible)
+                .click();
+
+        sleep(2000);
+
+        $x("//*[contains(text(), 'Continue')]")
+                .shouldBe(visible)
+                .click();
+
+        sleep(2000);
+
+        boolean shipSunken = false;
+        Random rand = new Random();
+
+        for (int i = 0; i < 50; i++) {
+            ElementsCollection availableCells = $$("td[class*='cell-']")
+                    .filterBy(cssClass("null"));
+
+            SelenideElement cell = availableCells.get(rand.nextInt(availableCells.size()));
+
+            cell.click();
+
+            sleep(500);
+
+            if($("img.is-destroyed").exists()) {
+                shipSunken = true;
+                break;
+            }
+        }
+
+        assertTrue(shipSunken, "Nenhum navio destruído");
+    }
+
+    void closePopup() {
         SelenideElement rejectButton = $x("//*[contains(text(),'not consent')]");
 
         if (rejectButton.exists()) {
